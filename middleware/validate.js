8ce4a95101f2d4b5c03d92b2d49d39df6fcbad6a
Felipe <felipesantos.dsb@hotmail.com>
2023-11-30 05:48:58 -0300

    order endpoints added

--- middleware/validate.js
+++ middleware/validate.js
@@ -24,6 +24,29 @@ const saveRestaurant = (req, res, next) => {
   });
 };
 
+const saveOrder = (req, res, next) => {
+  const validationRule = {
+    clientName: 'required|string',
+    price: 'required|numeric',
+    tableNumber: 'required|numeric',
+    staffName: 'required|string',
+    date: 'required|date',
+    orderStatus: 'required|string',
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err,
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+
 module.exports = {
-  saveRestaurant,
+  saveRestaurant, saveOrder
 };
