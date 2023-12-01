b93ad8a5993821679d6748dc1a85e6b228b7e84c
armando kamisaki <akamisaki@gmail.com>
2023-11-30 22:26:06 -0300

    Validate file updated

--- middleware/validate.js
+++ middleware/validate.js
@@ -26,6 +26,28 @@ const saveRestaurant = (req, res, next) => {
   });  
 };
 
+const saveOrder = (req, res, next) => {
+  const validationRule = {
+    clientName: 'required|string',
+    price: 'required|numeric',
+    tableNumber: 'required|numeric',
+    staffName: 'required|string',
+    date: 'required|date',
+    orderStatus: 'required|string',
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err,
+      });
+    } else {
+      next();
+    }
+  });
+};
+
 const saveStaff = (req, res, next) => {
   const validationStaffRule = {
     firstName: 'required|string',
@@ -53,5 +75,6 @@ const saveStaff = (req, res, next) => {
 
 module.exports = {
   saveRestaurant,
+  saveOrder,
   saveStaff,
 };
