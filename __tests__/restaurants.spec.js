307fb3101c24fc65660004b8a89df8b6d5ba73d6
Marianaeiterer <marieiterer@gmail.com>
2023-12-06 13:54:29 -0700

    jest mongodb configuration

--- __tests__/restaurants.spec.js
+++ __tests__/restaurants.spec.js
@@ -1,1 +1,24 @@
-import { getAllRestaurants } from '../controllers/restaurant';
+const mongoose = require('mongoose');
+const request = require('supertest');
+const express = require('express');
+const app = express();
+app.use('/', require('../routes/index'));
+
+require('dotenv').config();
+
+/* Connecting to the database before each test. */
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+});
+
+/* Closing database connection after each test. */
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+describe('GET /api-docs/restaurants', () => {
+  it('should return all restaurants', async () => {
+    const res = await request(app).get('/restaurants/');
+    expect(res.statusCode).toBe(200);
+  });
+});
