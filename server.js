8da69dcf996af86490bc5f64e44cc2d27641737c
Felipe <felipesantos.dsb@hotmail.com>
2023-12-08 00:10:32 -0300

    oauth

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const app = express();
 
 app
   .use(bodyParser.json())
+  .use(session({
+    secret: "secret",
+    resave: false,
+    saveUnInitialized: true,
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -19,7 +29,20 @@ app
     );
     next();
   })
+  .use(cors({methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PATCH']}))
+  .use(cors({origin: '*'}))
   .use('/', require('./routes/index'));
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done){
+  return done(null, profile);
+}
+));
+
 
 //handle errors
 process.on('uncaughtException', (err, origin) => {
@@ -34,5 +57,4 @@ mongodb.initDb((err) => {
     console.log(err);
   }
 });
-
 module.exports = app;
