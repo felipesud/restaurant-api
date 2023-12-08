32d97eb02dc7387d6ad6bc5e117bfb35ef65c8c1
ebelfel <felipe.belisario@ericsson.com>
2023-12-08 10:51:57 -0300

    menu router oauth setup

--- routes/menu.js
+++ routes/menu.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
 const menu = require('../controllers/menu');
-const validation = require('../middleware/validate');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', menu.getAllMenus);
 router.get('/:id', menu.getSingleMenu);
 
-router.post('/', validation.saveMenu, menu.createMenu);
+router.post('/', isAuthenticated, menu.createMenu);
 
-router.put('/:id', validation.saveMenu, menu.updateMenu);
+router.put('/:id', isAuthenticated, menu.updateMenu);
 
-router.delete('/:id', menu.deleteMenu);
+router.delete('/:id', isAuthenticated, menu.deleteMenu);
 
 module.exports = router;
