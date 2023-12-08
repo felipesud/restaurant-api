568a34d3123bfb6df25fe589842a1745124ef7e1
ebelfel <felipe.belisario@ericsson.com>
2023-12-08 10:51:48 -0300

    index router oauth setup

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const passport = require('passport');
 router.use('/', require('./swagger'));
 
 router.use('/clients', require('./client.routes'));
@@ -7,5 +7,14 @@ router.use('/staff', require('./staff'));
 router.use('/restaurants', require('./restaurant'));
 router.use('/menus', require('./menu'));
 router.use('/orders', require('./order'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err){
+        if(err) { return next(err); }
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
