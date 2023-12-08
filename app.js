e01ad3454792e98d6e3444745751705d714fcef5
ebelfel <felipe.belisario@ericsson.com>
2023-12-08 10:51:13 -0300

    configure oauth

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const port = process.env.PORT || 3000;
+const dotenv = require("dotenv").config();
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 const app = express();
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); 
+const restaurantRouter = require('./routes/restaurant')
 
 app
   .use(bodyParser.json())
+  .use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  }))
+  .use(passport.session())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -18,7 +30,42 @@ app
     );
     next();
   })
+  .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+  .use(cors({ origin: '*' }))
   .use('/', require('./routes/index'));
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+},
+  function (accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+})
+
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') });
+app.get('/github/callback', passport.authenticate('github', {
+  failureRedirect: '/api-docs', session: false
+}),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+
+  });
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+app.use('/restaurants', restaurantRouter);
 
 //handle errors
 process.on('uncaughtException', (err, origin) => {
