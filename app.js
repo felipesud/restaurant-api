c0774df5d390ab428477e5413372051d209e59d2
jonatanalmedia <jonatan.t.almedia@gmail.com>
2023-12-08 23:04:46 -0400

    authentication changes

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const passport = require("passport");
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 
 const app = express();
 
 app
   .use(bodyParser.json())
+  .use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  }))
+  .use(passport.session())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -18,7 +28,37 @@ app
     );
     next();
   })
+  .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+  .use(cors({ origin: '*' }))
   .use('/', require('./routes/index'));
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  ));
+  
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  })
+  
+  app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') });
+  app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false
+  }),
+    (req, res) => {
+      req.session.user = req.user;
+      res.redirect('/');
+  
+    });
+  
+  app.use(express.json());
 
 //handle errors
 process.on('uncaughtException', (err, origin) => {
