1ee58cc685fd21d4f4020587640bc36ffafd8da5
Marianaeiterer <marieiterer@gmail.com>
2023-12-09 10:30:45 -0700

    tests fixed

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-const passport = require("passport");
-const session = require('express-session');
-const GitHubStrategy = require('passport-github2').Strategy;
-const cors = require('cors');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 app
   .use(bodyParser.json())
-  .use(session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-  }))
-  .use(passport.session())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -28,45 +17,7 @@ app
     );
     next();
   })
-  .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
-  .use(cors({ origin: '*' }))
   .use('/', require('./routes/index'));
-  passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
-  },
-    function (accessToken, refreshToken, profile, done) {
-      return done(null, profile);
-    }
-  ));
-  
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  })
-  
-  app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') });
-  app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session: false
-  }),
-    (req, res) => {
-      req.session.user = req.user;
-      res.redirect('/');
-  
-    });
-  
-  app.use(express.json());
-
-//handle errors
-process.on('uncaughtException', (err, origin) => {
-  console.log(
-    process.stderr.fd,
-    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
-  );
-});
 
 mongodb.initDb((err) => {
   if (err) {
--- server.js
+++ server.js
@@ -1,6 +1,88 @@
 const app = require('./app');
+const express = require('express');
 const mongodb = require('./db/connect');
+
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
+
+app
+  .use(bodyParser.json())
+  .use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: true,
+    })
+  )
+  .use(passport.session())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'POST, GET, PUT, PATCH, OPTIONS, DELETE'
+    );
+    next();
+  })
+  .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+  .use(cors({ origin: '*' }))
+  .use('/', require('./routes/index'));
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get('/', (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : 'Logged Out'
+  );
+});
+app.get(
+  '/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/api-docs',
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  }
+);
+
+app.use(express.json());
+
+//handle errors
+process.on('uncaughtException', (err, origin) => {
+  console.log(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  );
+});
 
 mongodb.initDb((err) => {
   if (err) {
